55892e22e16772ecca1f0b5d33eb1cb279b8a10f
Osvaldo Cifuentes <osvaldoc@gmail.com>
2021-07-02 21:45:19 -0500

    Buscar por codigo

--- semana7/buscar_codigo_municipio.py
+++ semana7/buscar_codigo_municipio.py
@@ -7,11 +7,27 @@ Created on Fri Jul  2 11:59:51 2021
 
 import csv
 
-#Reto: Solicitar el codigo del municipio e imprimir el nombre, el departamento y la region
-
-# probar y adicionar en el open, encoding="utf-8"
-with open('C:/sources/misiontic/semana7/Departamentos_y_municipios_de_Colombia.csv', newline='',encoding="utf-8") as File:
+#Taller: Solicitar el codigo del municipio e imprimir el nombre municipio, 
+#el departamento y la region, sino existe el codigo, sacar advertencia
+with open('Departamentos_y_municipios_de_Colombia.csv', 
+          newline='',
+          encoding="utf-8") as File:
   reader = csv.reader(File)
+  contador = 0
+  codigo_municipio=input("Digite el municipio a buscar: ")
+  encontre_municipio=False
   for row in reader:
-      #Codificar aca utilizar comandos como input, split, print, condicional
-      print(row)
+      contador +=1
+      if row[3] == codigo_municipio:
+          print("Municipio:",row[4],"\nDepartamento:",row[2],"\nRegion:",row[0])
+          encontre_municipio = True
+  
+  #si es falso
+  if not encontre_municipio:      
+      print("no existe el municipio con ese codigo ",codigo_municipio )
+      
+  #-1 es porque encabezado
+  #print("Total municipios", contador-1)
+      
+#hasta las 9:30pm, probar con su municipio, para saber el codigo de su
+#municipio abrir el archivo en excel o buscarlo en el print de python
